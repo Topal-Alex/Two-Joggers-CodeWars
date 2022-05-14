6e0b10ef91dee34ba2f50064731449b7b0ce228e
Alex <79322837+Topal-Alex@users.noreply.github.com>
2022-05-14 15:16:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+
+namespace ConsoleApplication1
+{
+    class  Program
+    {
+        static void Main(string[] args)
+        {
+            NbrOfLaps(6, 3);
+
+        }
+        public static Tuple<int, int> NbrOfLaps(int x, int y)
+        {
+            int gcd = x;
+            while(gcd%y != 0)
+            {
+                gcd+=x;
+            }
+  
+            
+            
+            return new Tuple<int, int>(gcd/x, gcd/y);
+        }
+    }
+}
